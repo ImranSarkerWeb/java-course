fcbd17a4f3006c38ba3ab18de24e41e91da64cb7
imransarkerweb <imransarker.web@gmail.com>
2023-08-09 03:52:14 +0600

    grossPayCalculator

--- src/main/java/basics/GrossPayCalculator.java
+++ src/main/java/basics/GrossPayCalculator.java
@@ -1,4 +1,20 @@
 package basics;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
+    public static void main(String[] args){
+        System.out.println("How many hours did you work?");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        System.out.println("What is your hourly pay rate?");
+        double payRate = scanner.nextDouble();
+
+        double grossPay = hours * payRate;
+
+        scanner.close();
+        System.out.println("Gross pay " + grossPay);
+
+    }
 }
